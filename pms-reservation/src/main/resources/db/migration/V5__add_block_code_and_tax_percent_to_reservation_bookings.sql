3a9fe10bf215a4a0cc275069de31adce68ff53cf
pareshkumarl <pareshkumarl@cybage.com>
2026-09-09 12:59:40 +0530

    Fix SQL migration script by adding missing newline after block_code column definition

--- pms-reservation/src/main/resources/db/migration/V5__add_block_code_and_tax_percent_to_reservation_bookings.sql
+++ pms-reservation/src/main/resources/db/migration/V5__add_block_code_and_tax_percent_to_reservation_bookings.sql
@@ -1,3 +1,5 @@
 ALTER TABLE frontdeskdb.reservation_bookings
-    ADD COLUMN IF NOT EXISTS block_code VARCHAR(80),
+    ADD COLUMN IF NOT EXISTS block_code VARCHAR(80);
+
+ALTER TABLE frontdeskdb.reservation_bookings
     ADD COLUMN IF NOT EXISTS tax_percent NUMERIC(5,2);
